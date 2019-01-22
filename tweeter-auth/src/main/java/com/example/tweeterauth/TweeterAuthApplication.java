cdaf753887ad52ee5e3f2e42ebed29d47103ed45
ryo-murai <murai.ry@gmail.com>
2019-01-22 15:44:36 +0900

    create tweeter-auth project archetype

---
+++ tweeter-auth/src/main/java/com/example/tweeterauth/TweeterAuthApplication.java
@@ -0,0 +1,14 @@
+package com.example.tweeterauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweeterAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweeterAuthApplication.class, args);
+	}
+
+}
+
