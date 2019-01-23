ce80bb3f5ec219d9e4c696c5e8e32070356bf530
ryo-murai <murai.ry@gmail.com>
2019-01-23 14:27:44 +0900

    create webui-application project archetype

---
+++ tweeter-webui/src/main/java/com/example/tweeterwebui/TweeterWebuiApplication.java
@@ -0,0 +1,14 @@
+package com.example.tweeterwebui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweeterWebuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweeterWebuiApplication.class, args);
+	}
+
+}
+
