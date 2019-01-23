ce2c06dde99307138093418e23202ce997679167
ryo-murai <murai.ry@gmail.com>
2019-01-23 11:10:56 +0900

    create tweeter-cli project archetype

---
+++ tweeter-cli/src/main/java/com/example/tweetercli/TweeterCliApplication.java
@@ -0,0 +1,14 @@
+package com.example.tweetercli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweeterCliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweeterCliApplication.class, args);
+	}
+
+}
+
